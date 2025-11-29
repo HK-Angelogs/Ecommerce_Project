2c61211ca21ae2385270777df65454e5e7cfb913
“HK-Angelogs” <tomulinangelo24@gmail.com>
2025-11-30 05:04:12 +0800

    Added Registration.html

--- ecommerce_django/ecommerce_app/urls.py
+++ ecommerce_django/ecommerce_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views 
+from django.contrib import admin
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('', views.home_page, name='home_page'),
     path('', views.home_page, name='home_page'),
     path('cart/', views.cart_page, name='cart_page'),
     path('bestseller/', views.bestseller_page, name='bestseller_page'),
@@ -10,5 +13,7 @@ urlpatterns = [
     path('404/', views.error_page, name='404_page'),
     path('single/', views.single_page, name='single_page'),
     path('contact/', views.contact_page, name='contact_page'),
-    path('Login.html', views.Login_page, name='Login_page'),
+    path('login/', views.Login_page, name='Login_page'),
+    path('logout/', views.Logout_page, name='Logout_page'),
+    path('register/', views.register_user, name='register'),
 ]
--- ecommerce_django/ecommerce_app/views.py
+++ ecommerce_django/ecommerce_app/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from.forms import SignUpForm
+
 
 # Create your views here.
 def home_page (request):
     return render(request, 'index.html', {} )
+
+def Base_file (request):
+    return render(request, 'Base.html', {} )
+
+def Nav_page(request):
+    return render(request, 'Nav.html', {} )
 
 def cart_page(request):
     return render(request, 'cart.html', {} )
@@ -27,4 +41,44 @@ def single_page(request):
     return render(request, 'single.html', {})
 
 def Login_page(request):
+    if request.user.is_authenticated:
+        return redirect('home_page')  # prevents loop
+    
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login successful.")
+            return redirect('home_page')
+        else:
+            messages.error(request, "Invalid username or password.")
+            return redirect('Login_page')
+
     return render(request, 'Login.html', {})
+
+def Logout_page(request):   
+    logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect('home_page')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            # Login
+            user = authenticate(username=username, password=password)   
+            login(request, user)
+
+            messages.success(request, "Registration successful. You can now log in.")
+            return redirect('Login_page')
+        else:
+            messages.error(request, "Unsuccessful registration. Invalid information.")
+            return render(request, 'Registration.html', {'form': form})
+        
+    return render(request, 'Registration.html', {'form': form})
