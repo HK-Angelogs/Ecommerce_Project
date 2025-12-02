0dc635914f4b7bead730aff7aa69ae8e6d58dc13
“HK-Angelogs” <tomulinangelo24@gmail.com>
2025-12-02 14:17:34 +0800

    12/2/25 Updated Lots of Things

--- ecommerce_django/EcommerceProducts/models.py
+++ ecommerce_django/EcommerceProducts/models.py
@@ -28,7 +28,7 @@ class Products(models.Model):
     name = models.CharField(max_length=50)
     price = models.DecimalField(default=0, decimal_places=2, max_digits=10)
     category = models.ForeignKey(Categories, on_delete=models.CASCADE, default =1)
-    description = models.CharField(max_length=250,default='', blank = True, null = True)
+    description = models.TextField(default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
 
     def __str__(self):
